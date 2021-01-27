cc59640aa827e005206a1551b45eabbdca1dc529
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-27 16:25:06 -0600

    Update App file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="jumbotron">
+      <titulo v-bind:titulo="titulo"></titulo>
+      {{numTareas}}
+      <nueva-tarea v-bind:tareas="tareas" v-on:incrementarContador="numTareas += $event"></nueva-tarea>
+      <lista-tareas v-bind:tareas="tareas"></lista-tareas>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Titulo from './TituloComponent.vue'
+import NuevaTarea from './nuevaTareaComponent.vue'
+import ListaTareas from './ListaTareasComponent.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+      Titulo,
+      NuevaTarea,
+      ListaTareas
+    },
+    data(){
+      return {
+        titulo: 'Lista de Tareas',
+        numTareas: 3,
+        tareas: [
+          {
+                texto: 'Aprender Vue.js',
+                terminada: false
+            },
+            {
+                texto: 'Aprender Angular 2',
+                terminada: false
+            },
+            {
+                texto: 'Aprender ionic',
+                terminada: false
+            }
+        ]
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
