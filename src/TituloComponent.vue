9d4b8347e79950b8180be0636b6d779cae84c077
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-27 16:23:36 -0600

    Add title component file

---
+++ src/TituloComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h2>{{tituloMayusculas()}}</h2>
+    <h3>Tareas a hacer:</h3>
+</div>    
+</template>
+<script>
+export default {
+    props: {
+        titulo: {
+            type: String,
+            default: 'Tareas'
+        }
+    },
+    methods: {
+        tituloMayusculas(){
+            return this.titulo.toUpperCase()
+        }
+    }
+}
+</script>
