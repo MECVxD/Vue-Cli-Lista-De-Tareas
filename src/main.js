22a6c08d148c068a8f05d1b319397dd1ff2e0627
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 22:14:37 -0600

    Update index files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+Vue.http.options.root='https://tareas-ef1b3-default-rtdb.firebaseio.com/'
 
 export var bus=new Vue({
   methods:{
