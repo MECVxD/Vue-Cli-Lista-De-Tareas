dad365b562680bd57f4dfa696b71f4908994a975
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 22:12:55 -0600

    Create task-list component

--- src/ListaTareasComponent.vue
+++ src/ListaTareasComponent.vue
@@ -6,7 +6,7 @@
                 <span class="pull-right">
                     <button type="button" 
                             class="btn btn-success btn-xs glyphicon glyphicon-ok"
-                            v-on:click="tarea.terminada = !tarea.terminada"></button>
+                            v-on:click="estado(indice)"></button>
                 <button type="button" 
                         class="btn btn-danger btn-xs glyphicon glyphicon-remove"
                         v-on:click="borrar(indice)"></button>        
@@ -21,8 +21,18 @@ import { bus } from './main.js'
 export default {
     props:['tareas'],
     methods: {
+        estado(indice){
+          let terminada = this.tarea[indice].terminada= !this.tareas[indice].terminada
+          let id = this.tareas[indice].id
+          
+          this.$http.patch('tareas/'+id+'.json',{
+              terminada:terminada
+          }).then(respuesta=>{console.log(respuesta)})  
+        },
         borrar: function(indice){
+            let id = this.tareas[indice].id
             this.tareas.splice(indice,1)
+            this.$http.delete('tareas/'+id+'.json').then(respuesta=>{console.log(respuesta)})
         }
     },
     created(){
